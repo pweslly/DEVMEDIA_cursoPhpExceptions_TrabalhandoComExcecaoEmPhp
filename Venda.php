3dc68ffa872acb8fa2cbae0dc09f5c8fe1b2edf6
Pw <pablo_weslly@hotmail.com>
2019-08-15 21:59:57 -0300

    #3 - Aula 4 - Fiz modificações no front end e também queria exibir nome do produto

---
+++ Venda.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Venda;
+use Produto\Produto;
+
+class Venda extends Produto{
+
+  private $itens = [];
+
+  
+  public function adicionar($Produto){
+    if($Produto == null){
+      throw new \InvalidArgumentException("Item não pode ser nulo");
+    }
+      array_push($this->itens, $Produto);
+   
+  }
+
+    public function exibirProduto(){
+    $ArrayProduto = $this->itens;    
+    $QtdValordArrayProduto = count($ArrayProduto);
+    $j = $QtdValordArrayProduto;   
+    $nomeProduto = "";     
+      for($i = 1; $i < $j; $i++){                     
+        foreach($this->itens as  $itemtwo){          
+        $nomeProduto  .= $itemtwo->getDescricao()."<br>";      
+        }          
+    } 
+    return  $nomeProduto;      
+  }
+
+
+  public function exibirPreco(){
+    $ArrayProduto = $this->itens;    
+    $QtdValordArrayProduto = count($ArrayProduto);
+    $j = $QtdValordArrayProduto;   
+    $informacaodoPreco = "";     
+      for($i = 1; $i < $j; $i++){                     
+        foreach($this->itens as  $itemtwo){          
+        $informacaodoPreco  .= $itemtwo->getPreco()."<br>";      
+        }          
+    } 
+    return  $informacaodoPreco;      
+  }
+
+  public function getTotal(){
+    $total = 0;  
+    foreach($this->itens as $item){
+      $total += $item->getPreco();
+      
+    }
+    return $total;
+  }
+}
--- index.php
+++ index.php
@@ -1,20 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+  <title>Assinatura - Criando uma  Class OOP</title> 
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
 <?php 
-  use ItemVenda\ItemVenda;
+
+  use Produto\Produto;
   use Venda\Venda;
 
-  require_once "Venda.php";
-  require_once "ItemVenda.php";
+ 
+  include_once ("Produto.php");
+  include_once ("Venda.php");
   
-  $camisa = new ItemVenda();
-  $camisa->setDescricao("Camisa Polo");
-  $camisa->setPreco(12.0);
+  try{
+  $Camisa = new Produto();
+  
+  $Camisa->setPreco("12");
+  $Camisa->setDescricao("Camisa Polo");
 
-  $bone = new ItemVenda();
-  $bone->setDescricao("Boné Infantil");
-  $bone->setPreco(21.0);
+  $Bone = new Produto();
+
+  $Bone->setPreco("5");
+  $Bone->setDescricao("Bone X-men");
 
   $Venda = new Venda();
-  $Venda->adicionar(null);
-  $Venda->adicionar($bone);
+  $Venda->adicionar($Camisa);
+  $Venda->adicionar($Bone);
+  ?>
 
-  print "Total da venda: ". $Venda->getTotal();
+  <div class="container">  
+    <div classs="row">
+      <div class="col-md-4 order-md-2 mb-4">
+          <h4 class="d-flex justify-content-between align-items-center mb-3">
+            <span class="text-muted">Meu Carrinho</span>
+            <span class="badge badge-secondary badge-pill"></span>
+          </h4>
+          <ul class="list-group mb-3">
+            <li class="list-group-item d-flex justify-content-between lh-condesed">
+              <div>
+              <h6 class="my-0"><?php echo $Venda->exibirProduto(); ?></h6>
+                <small class="text-muted"></small>
+            </div>
+
+  <span class="text-muted"><?php echo "Total: ". $Venda->getTotal(); ?></span>
+  <?php
+    } catch (\InvalidArgumentException $e){
+      echo "Error Encontrado: " . $e->getMessage();
+    }
+?>         
+            </li>
+
+
+            <li class="list-group-item d-flex justify-content-between lh-condesed">
+              <div>
+                <h6 class="my-0">Preço Unitário</h6>
+                <small class="text-muted">Preço Unitário</small>
+              </div>
+              <span class="text-muted"><?php echo $Venda->exibirPreco(); ?></span>
+            </li>
+            <button class="btn btn-primary btn-lg btn-block" type="submit">Assinar</button>
+        </ul>
+  </div>
+
+
+
+  </div>
+  
+    </body>
+    </html>
